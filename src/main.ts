57fd133c8f1a13717e3669ed34a770287f4d8f54
Edwin Alexander Rendon <36104986+eRendon@users.noreply.github.com>
2021-04-01 20:38:08 -0500

    fix routes vite dev

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,7 @@
 import { createApp } from 'vue'
 import { firestorePlugin } from 'vuefire';
 
-import MainLayout from '@/layout/MainLayout/MainLayout.vue';
-import Skeleton from '@/components/shared/SkeletonLoading/Skeleton.vue';
-import DetailItem from '@/components/shared/Modals/DetailItem/DetailItem.vue';
-import Toast from '@/components/shared/Toast/Toast.vue';
 import App from './App.vue'
-
-import './registerServiceWorker';
 
 /* router */
 import router from './router';
@@ -17,6 +11,12 @@ import './assets/styles/index.css';
 import './assets/scss/styles.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "@/assets/styles/tailwind.css";
+
+/* components */
+import MainLayout from '@/layout/MainLayout/MainLayout.vue';
+import Skeleton from '@/components/shared/SkeletonLoading/Skeleton.vue';
+import DetailItem from '@/components/shared/Modals/DetailItem/DetailItem.vue';
+import Toast from '@/components/shared/Toast/Toast.vue';
 
 const app = createApp(App).use(router)
 
@@ -29,4 +29,5 @@ app.component('toast', Toast);
 /* plugins */
 app.use(firestorePlugin);
 
-app.mount('#app');
+/* mount app*/
+app.mount('#app')
