23b4965818dd1e2237a4da7681d4932492d9be3c
Edwin Alexander Rendon <36104986+eRendon@users.noreply.github.com>
2021-10-14 11:58:23 -0500

    update to chart 3.5

--- src/components/Cards/CardBarChart/CardBarChart.ts
+++ src/components/Cards/CardBarChart/CardBarChart.ts
@@ -1,108 +1,120 @@
-import { defineComponent } from 'vue';
-import Chart from 'chart.js';
+import { defineComponent, nextTick, onMounted } from 'vue';
+import {
+  Chart,
+  ChartConfiguration,
+  CategoryScale,
+  LinearScale,
+  BarController,
+  BarElement,
+  LineElement,
+  LineController,
+  PointElement,
+  Tooltip,
+  Legend, Title
+} from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, BarController, BarElement, LineElement, LineController, PointElement, Tooltip, Legend, Title)
+
 
 export default defineComponent({
   name: 'CardBarChart',
-  async mounted() {
-    await this.$nextTick(() => {
-      const config = {
-        type: 'bar',
-        data: {
-          labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-          ],
-          datasets: [
-            {
-              label: new Date().getFullYear(),
-              backgroundColor: '#ed64a6',
-              borderColor: '#ed64a6',
-              data: [30, 78, 56, 34, 100, 45, 13],
-              fill: false,
-              barThickness: 8,
-            },
-            {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: '#4c51bf',
-              borderColor: '#4c51bf',
-              data: [27, 68, 86, 74, 10, 4, 87],
-              barThickness: 8,
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
+  setup() {
+    const config: ChartConfiguration = {
+      type: 'bar',
+      data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+        ],
+        datasets: [
+          {
+            label: new Date().getFullYear().toString(),
+            backgroundColor: '#ed64a6',
+            borderColor: '#ed64a6',
+            data: [30, 78, 56, 34, 100, 45, 13],
+            fill: false,
+            barThickness: 8,
+          },
+          {
+            label: (new Date().getFullYear() - 1).toString(),
+            fill: false,
+            backgroundColor: '#4c51bf',
+            borderColor: '#4c51bf',
+            data: [27, 68, 86, 74, 10, 4, 87],
+            barThickness: 8,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
           title: {
             display: false,
             text: 'Orders Chart',
           },
-          tooltips: {
+          tooltip: {
             mode: 'index',
             intersect: false,
           },
-          hover: {
-            mode: 'nearest',
-            intersect: true,
-          },
           legend: {
             labels: {
-              fontColor: 'rgba(0,0,0,.4)',
+              color: 'rgba(0,0,0,.4)',
             },
             align: 'end',
             position: 'bottom',
           },
-          scales: {
-            xAxes: [
-              {
-                display: false,
-                scaleLabel: {
-                  display: true,
-                  labelString: 'Month',
-                },
-                gridLines: {
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: 'rgba(33, 37, 41, 0.3)',
-                  zeroLineColor: 'rgba(33, 37, 41, 0.3)',
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: 'Value',
-                },
-                gridLines: {
-                  borderDash: [2],
-                  drawBorder: false,
-                  borderDashOffset: [2],
-                  color: 'rgba(33, 37, 41, 0.2)',
-                  zeroLineColor: 'rgba(33, 37, 41, 0.15)',
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
         },
-      };
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
+        scales: {
+          x: {
+              display: false,
+              title: {
+                display: true,
+                text: 'Month',
+              },
+              grid: {
+                borderDash: [2],
+                borderDashOffset: 2,
+                color: 'rgba(33, 37, 41, 0.3)',
+              },
+            },
+          y: {
+              display: true,
+              title: {
+                display: false,
+                text: 'Value',
+              },
+              grid: {
+                borderDash: [2],
+                drawBorder: false,
+                borderDashOffset: 2,
+                color: 'rgba(33, 37, 41, 0.2)',
+              },
+            },
+        },
+      },
+    }
+
+    const renderChart = ():void => {
       // @ts-ignore
       const ctx = document.getElementById('bar-chart')!.getContext('2d');
       // @ts-ignore
-      window.myBar = new Chart(ctx, config);
-    });
-  },
-  setup() {
+      window.myLine = new Chart(ctx, config);
+    }
+
+    onMounted(async () => {
+      await nextTick(() => {
+        renderChart()
+      })
+    })
   },
 });
--- src/components/Cards/CardLineChart/CardLineChart.ts
+++ src/components/Cards/CardLineChart/CardLineChart.ts
@@ -1,114 +1,128 @@
-import { defineComponent } from 'vue';
-import Chart from 'chart.js';
+import { defineComponent, nextTick, onMounted } from 'vue';
+import {
+  Chart,
+  ChartConfiguration,
+  CategoryScale,
+  LinearScale,
+  BarController,
+  BarElement,
+  LineElement,
+  LineController,
+  PointElement,
+  Tooltip,
+  Legend, Title
+} from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, BarController, BarElement, LineElement, LineController, PointElement, Tooltip, Legend, Title)
 
 export default defineComponent({
   name: 'CardLineChart',
-  async mounted() {
-    await this.$nextTick(() => {
-      const config = {
-        type: 'line',
-        data: {
-          labels: [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-          ],
-          datasets: [
-            {
-              label: new Date().getFullYear(),
-              backgroundColor: '#4c51bf',
-              borderColor: '#4c51bf',
-              data: [65, 78, 66, 44, 56, 67, 75],
-              fill: false,
-            },
-            {
-              label: new Date().getFullYear() - 1,
-              fill: false,
-              backgroundColor: '#fff',
-              borderColor: '#fff',
-              data: [40, 68, 86, 74, 56, 60, 87],
-            },
-          ],
-        },
-        options: {
-          maintainAspectRatio: false,
-          responsive: true,
+  setup() {
+    const config: ChartConfiguration = {
+      type: 'line',
+      data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+        ],
+        datasets: [
+          {
+            label: new Date().getFullYear().toString(),
+            backgroundColor: '#4c51bf',
+            borderColor: '#4c51bf',
+            data: [65, 78, 66, 44, 56, 67, 75],
+            fill: false,
+          },
+          {
+            label: (new Date().getFullYear() - 1).toString(),
+            fill: false,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            data: [40, 68, 86, 74, 56, 60, 87],
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        plugins: {
           title: {
             display: false,
             text: 'Sales Charts',
-            fontColor: 'white',
+            color: 'white',
           },
           legend: {
             labels: {
-              fontColor: 'white',
+              color: 'white',
             },
             align: 'end',
             position: 'bottom',
           },
-          tooltips: {
+          tooltip: {
             mode: 'index',
             intersect: false,
           },
-          hover: {
-            mode: 'nearest',
-            intersect: true,
-          },
-          scales: {
-            xAxes: [
-              {
-                ticks: {
-                  fontColor: 'rgba(255,255,255,.7)',
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: 'Month',
-                  fontColor: 'white',
-                },
-                gridLines: {
-                  display: false,
-                  borderDash: [2],
-                  borderDashOffset: [2],
-                  color: 'rgba(33, 37, 41, 0.3)',
-                  zeroLineColor: 'rgba(0, 0, 0, 0)',
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  fontColor: 'rgba(255,255,255,.7)',
-                },
-                display: true,
-                scaleLabel: {
-                  display: false,
-                  labelString: 'Value',
-                  fontColor: 'white',
-                },
-                gridLines: {
-                  borderDash: [3],
-                  borderDashOffset: [3],
-                  drawBorder: false,
-                  color: 'rgba(255, 255, 255, 0.15)',
-                  zeroLineColor: 'rgba(33, 37, 41, 0)',
-                  zeroLineBorderDash: [2],
-                  zeroLineBorderDashOffset: [2],
-                },
-              },
-            ],
-          },
         },
-      };
+        hover: {
+          mode: 'nearest',
+          intersect: true,
+        },
+        scales: {
+          x: {
+              ticks: {
+                color: 'rgba(255,255,255,.7)',
+              },
+              display: true,
+              title: {
+                display: false,
+                text: 'Month',
+                color: 'white',
+              },
+              grid: {
+                display: false,
+                borderDash: [2],
+                borderDashOffset: 2,
+                color: 'rgba(33, 37, 41, 0.3)',
+              },
+            },
+          y: {
+              ticks: {
+                color: 'rgba(255,255,255,.7)',
+              },
+              display: true,
+              title: {
+                display: false,
+                text: 'Value',
+                color: 'white',
+              },
+              grid: {
+                borderDash: [3],
+                borderDashOffset: 3,
+                drawBorder: false,
+                color: 'rgba(255, 255, 255, 0.15)',
+              },
+            },
+        },
+      },
+    }
+
+    const renderChart = ():void => {
       // @ts-ignore
       const ctx = document.getElementById('line-chart')!.getContext('2d');
       // @ts-ignore
       window.myLine = new Chart(ctx, config);
-    });
-  }
+    }
+
+    onMounted(async () => {
+      await nextTick(() => {
+        renderChart()
+      })
+    })
+
+  },
 })
