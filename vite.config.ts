2a8073fc3d97a856e422a86cb4c2add16d6443f8
harbard <devfront@dragonhunt.io>
2022-02-23 09:01:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-      ? '/vite-vue3-tailwind/'
-      : '/',
   define: {
     'process.env': {}
   },
