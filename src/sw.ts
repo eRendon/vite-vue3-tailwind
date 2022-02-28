5de5d12ee69d00b6a0c4b6c82654e30123fb3108
harbard <devfront@dragonhunt.io>
2022-02-28 08:22:23 -0500

    Update sw.ts

--- src/sw.ts
+++ src/sw.ts
@@ -1,20 +1,20 @@
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-declare let self: ServiceWorkerGlobalScope
-
-const manifest = self.__WB_MANIFEST
-
-console.log(process)
-
-if (process.env.NODE_ENV === 'production') {
-  precacheAndRoute(manifest)
-  self.skipWaiting()
-}
-clientsClaim()
-
-cleanupOutdatedCaches()
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
-})
+// import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
+// import { clientsClaim } from 'workbox-core'
+// declare let self: ServiceWorkerGlobalScope
+//
+// const manifest = self.__WB_MANIFEST
+//
+// console.log(process)
+//
+// if (process.env.NODE_ENV === 'production') {
+//   precacheAndRoute(manifest)
+//   self.skipWaiting()
+// }
+// clientsClaim()
+//
+// cleanupOutdatedCaches()
+//
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'SKIP_WAITING')
+//     self.skipWaiting()
+// })
