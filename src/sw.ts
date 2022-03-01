c77ec3cf3651a762b30a1fa845d3c9ec0974d706
harbard <devfront@dragonhunt.io>
2022-03-01 08:31:30 -0500

    Update sw.ts

--- src/sw.ts
+++ src/sw.ts
@@ -1,3 +1,4 @@
+/// <reference lib="webworker" />
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
 declare let self: ServiceWorkerGlobalScope
